25805b968b37bd14edc3c276755f9bb773e9ea67
Frank Faustino <nawzyah@gmail.com>
2018-07-25 16:39:09 -0700

    Refactor for GeoFire

--- src/controllers/drivers.js
+++ src/controllers/drivers.js
@@ -1,17 +1,35 @@
-import { admin, auth, db } from '../config/firebase'
+import { admin, auth, db, geoFire } from '../config/firebase'
 import coords from './coords'
 
-const drivers = coords.map((pos, i) => ({ position: pos, id: i }));
+async function populateDrivers() {
+  try {
+    const obj = coords.reduce((a, p, i) => ({ ...a,
+      [`driver:${i}`]: [p.latitude, p.longitude]
+    }), {})
+    await geoFire.set(obj)
+  } catch (e) { console.log(e) }
+}
 
-function populateDrivers() {
-  db.ref('drivers').remove()
+async function queryDrivers() {
+  const query = await geoFire.query({
+    center: [35.045630, -85.309680],
+    radius: 10
+  })
 
-  drivers.forEach(driver => {
-    db.ref('drivers').child(driver.id).update(driver)
+  query.on('key_entered', (key, location, distance) => {
+    console.log(`${key} entered query at ${location} ${distance} km from center`)
+  })
+
+  query.on('key_moved', (key, location, distance) => {
+    console.log(`${key} moved within query to ${location} ${distance} km from center`)
+  })
+
+  query.on('key_exited', (key, location, distance) => {
+    console.log(`${key} exited query to ${location} ${distance} km from center`)
   })
 }
 
-function randomPosition(lat, lon) {
+function randomPosition(pos) {
   const operations = [
     '+ 0.00005',
     '- 0.00005',
@@ -22,24 +40,24 @@ function randomPosition(lat, lon) {
   ]
   const j = Math.random() * operations.length | 0
 
-  return {
-    latitude: eval(`${lat} ${operations[j]}`),
-    longitude: eval(`${lon} ${operations[j]}`),
-  }
+  return [
+    eval(`${pos[0]} ${operations[j]}`),
+    eval(`${pos[1]} ${operations[j]}`)
+  ]
 }
 
-function updateRandomDriver() {
-  const i = Math.random() * drivers.length | 0
-  const updatedDriver = drivers[i]
-  const { position: { latitude, longitude } } = updatedDriver
-
-  updatedDriver.position = randomPosition(latitude, longitude)
-
-  db.ref('drivers').child(drivers[i].id).update(updatedDriver)
+async function updateRandomDriver() {
+  try {
+    const i = Math.random() * coords.length | 0
+    const driver = await geoFire.get(`driver:${i}`)
+    const newPos = randomPosition(driver)
+    await geoFire.set({ [`driver:${i}`]: newPos })
+  } catch (e) { console.log(e) }
 }
 
 export function main() {
-  populateDrivers()
+  queryDrivers()
+  // populateDrivers()
 
   setInterval(() => {
     updateRandomDriver()
